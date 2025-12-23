b9ec3b7dc095cee912035c9bf44bde84d1e3c8f8
Nathan <njsecord@lakeheadu.ca>
2025-12-23 01:11:49 -0500

    came up with a wayyy better solution (performance wise) for set mismatch problem

--- leetcode problems/set_mismatch2.cpp
+++ leetcode problems/set_mismatch2.cpp
@@ -1,32 +1,63 @@
 #include<iostream> 
 #include<vector> 
 #include<unordered_map> 
+#include<cmath> 
 
 using namespace std; 
 
 vector<int> findErrorNums(vector<int>& nums){
 
     unordered_map<int, int> hashmap;
-    vector<int> result;
+    vector<int> result (2);
     int current_num = 0; 
     bool flag = false; 
+    int sum = 0; 
+    int err_sum = 0; 
 
     for(int i = 0; i < nums.size(); i++)
     {
-        current_num = nums[i]; 
-        if(hashmap.find(current_num) != hashmap.end()) // find will return end iterator if whatever is being searched for cannot be found. 
+        current_num = nums[i];                       
+        if(hashmap.find(current_num) == hashmap.end()) //.end() means cannot be found  
         {
             // add to hashmap 
-            hashmap[current_num] = i; // <current_num, index> 
+            hashmap[current_num] = i; // <current_num, index>             
 
         }else{
             // dupe found 
             result[0] = current_num;
             flag = true; 
-            break; 
+            break;
         }
         if(flag == true)
             break; 
+    }
+
+    if(nums.size() > 2){
+        // real sum
+        for(int i = 1; i <= nums.size(); i++)
+        {
+            sum += i; 
+        }
+
+        // sum error-ed array 
+        for(int i = 0; i < nums.size(); i++)
+        {
+            err_sum += nums[i]; 
+        }
+
+        // calculate missing value 
+        int delta_dupe = err_sum - result[0]; // now we have removed the dupe value from the error-ed sum, and can find the detla between the actual sum and the sum missing a value
+        int delta = abs(sum - delta_dupe); // take absolute to ensure not negative 
+        result[1] = delta; 
+
+    }else{
+        if(result[0] == 1)
+        {
+            result[1] = result[0] + 1;
+        }else{
+            result[1] = result[0] - 1; 
+        }
+        
     }
 
     return result; 
@@ -34,5 +65,14 @@ vector<int> findErrorNums(vector<int>& nums){
 }
 
 int main(){
+
+    vector<int> nums = {4,5,6,7,4,9}; 
+    vector<int> result = findErrorNums(nums); 
+    cout << "result: "; 
+    for(int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << ",";  
+    }
+    cout << endl; 
 
 }
