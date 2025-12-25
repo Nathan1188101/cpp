311a99d779501e34973fbb5fad7743491dfb49eb
Nathan <njsecord@lakeheadu.ca>
2025-12-25 02:24:02 -0500

    success, eval reverse_polish_notation

--- leetcode problems/eval_reverse_polish_notation.cpp
+++ leetcode problems/eval_reverse_polish_notation.cpp
@@ -24,34 +24,52 @@ int evalRPN(vector<string>& tokens){
         // if not pop last two and do operation
         // then push back 
 
+
+        char current_char = tokens[i][0];
+        cout << "at " << i << " we have: " << current_char << endl; 
+
                
         if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*")
         {
-            char current_char = tokens[i][0];
 
             // need to pop two most recent nums from stack here (order matters)
             int first = stack.back();
             stack.pop_back(); // delete
+            cout << "====" << endl; 
+            cout << "level: " << i << endl; 
+            cout << "first: " << first << endl; 
 
             int second = stack.back(); 
             stack.pop_back(); // delete
+            cout << "====" << endl; 
+            cout << "level: " << i << endl; 
+            cout << "second: " << first << endl; 
+            cout << "====" << endl; 
 
             switch(current_char){
 
                 case '+':
                     operation_result = first + second;
+                    cout << first << " + " << second << endl; 
+                    break;
 
                 case '-':
                     operation_result = second - first; 
+                    cout << second << " - " << first << endl;
+                    break; 
                 
                 case '*':
                     operation_result = first * second; 
+                    cout << first << " + " << second << endl;
+                    break;
 
                 case '/':
                     operation_result = second / first; 
+                    cout << second << " / " << first << endl; 
+                    break; 
 
             }
-
+            cout << "result of operation: " << operation_result << endl; 
             stack.push_back(operation_result);
 
         }else{ // else it's a num
