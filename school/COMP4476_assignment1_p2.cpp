9a3399a264c8dc9ae333f811ffb6d284a69bf966
U-NATHANPC\zopbo <njsecord@lakeheadu.ca>
2026-01-22 13:22:53 -0500

    working on input validation

--- school/COMP4476_assignment1_p2.cpp
+++ school/COMP4476_assignment1_p2.cpp
@@ -1,8 +1,10 @@
 #include<iostream> 
 #include<string> 
 #include<vector>
+#include<algorithm>
+#include<iterator> 
 
-std::string permutationCipher(const std::string& plain_text, const int& m){
+std::string permutationCipher(const std::string& plain_text, const int& m, const std::vector<int>& permutation){
 
     // we split plain text up into m sized blocks 
     // then make permutations of each block 
@@ -39,14 +41,40 @@ std::string permutationCipher(const std::string& plain_text, const int& m){
 
 int main(){
 
-    std::string plain_text; 
-    int m; 
+    int m; // block size
+    int input; // for permutation
+    std::vector<int> permutation; // key 
+    std::string plain_text; // message 
 
     std::cout << "enter message: "; 
     std::getline(std::cin, plain_text);
-    std::cout << "enter block size: "; 
+    std::cout << "enter block size (m > 0): "; 
     std::cin >> m; 
+    while (m <= 0){
+        std::cout << "block size must be greater than 0. Try again:";
+        std::cin >> m; 
+    }
 
-    std::string encrypt = permutationCipher(plain_text, m); 
+    std::cout << "enter permutation from 1 - " << m; 
+
+    while (permutation.size() != m) {
+
+        std::cin >> input; 
+
+        if (input > m || input <= 0) {
+            std::cout << "only input numbers from 1 - " << m << std::endl; 
+        }
+        else if (std::find(permutation.begin(), permutation.end(), input) != permutation.end()) {
+            // if != .end(), then it already exists. .end() is returned when what you're looking for is not there. 
+            std::cout << "Cannot add values already in key... try again." << std::endl; 
+        }
+        else {
+            // only adding if it passes validation 
+            permutation.push_back(input); 
+        }
+
+    } 
+
+    std::string encrypt = permutationCipher(plain_text, m, permutation); 
 
 }
