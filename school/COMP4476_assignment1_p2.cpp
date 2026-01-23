3dedf0e7d505c8a99e495b6ac72b3d0a1f9b56af
U-NATHANPC\zopbo <njsecord@lakeheadu.ca>
2026-01-22 20:49:25 -0500

    got it all working, except I'm not sure what to do about the padding. Leave it? remove it on decryption?

--- school/COMP4476_assignment1_p2.cpp
+++ school/COMP4476_assignment1_p2.cpp
@@ -10,6 +10,8 @@ std::string permutationCipher(std::string& plain_text, const int& m, const std::
 
     // removing the spaces in the message for clean blocks
     plain_text.erase(std::remove(plain_text.begin(), plain_text.end(), ' '), plain_text.end()); 
+
+    int length = plain_text.size();
     
     // string vector for blocks 
     std::vector<std::string> blocks; 
@@ -66,10 +68,10 @@ std::string permutationCipher(std::string& plain_text, const int& m, const std::
         for (int i = 0; i < current_block.size(); i++) {
 
             // look up what i should be in permutation 
-            int location = permutation[i] - 1; // need to - 1 because of 0 based counting 
+            int index = permutation[i] - 1; // need to - 1 because of 0 based counting 
 
             // then go to that location in the block and add that char to the new block
-            permuted_blocks.back().push_back(current_block[location]); // build new permuted block into empty block
+            permuted_blocks.back().push_back(current_block[index]); // build new permuted block into empty block
 
         }
     }
@@ -89,6 +91,70 @@ std::string permutationCipher(std::string& plain_text, const int& m, const std::
     }
     
     return encrypted_message;
+
+}
+
+std::string permutationCipherDecryption(std::string& cipher_text, const int& m, const std::vector<int>& permutation){
+
+    std::string plain_text; 
+
+    // need to split the cipher text into m sized blocks 
+    int counter = 1; 
+    int current = 0; 
+    std::vector<std::string> permuted_blocks;  
+    permuted_blocks.push_back(""); // start empty block 
+    for (int i = 0; i < cipher_text.size(); i++) {
+        
+        if (counter <= m) {
+            counter++; 
+            permuted_blocks[current].push_back(cipher_text[i]);
+        } else {
+            counter = 1; 
+            current ++; 
+            permuted_blocks.push_back(""); // start new block 
+            permuted_blocks.back().push_back(cipher_text[i]); 
+            counter ++; 
+        }
+    }
+
+    // get inverse of permutation 
+    std::vector<int> inverse_permutation;
+    for (int i = 0; i < permutation.size(); i++) {
+
+        // find num in permutation 
+        auto num = std::find(permutation.begin(), permutation.end(), i + 1); 
+
+        // get index 
+        int index = std::distance(permutation.begin(), num); 
+
+        inverse_permutation.push_back(index + 1); 
+
+    }
+
+    // now we have permuted blocks 
+    // need to revert them using inverse permutation  
+    std::vector<std::string> blocks; 
+    for (int i = 0; i < permuted_blocks.size(); i++) {
+
+        std::string current_block = permuted_blocks[i];
+        blocks.push_back("");  
+
+        for (int i = 0; i < current_block.size(); i++) {
+            // look up what i should be in permutation 
+            int index = inverse_permutation[i] - 1; // need to - 1 because of 0 based counting 
+
+            // then go to that location in the block and add that char to the new block
+            blocks.back().push_back(current_block[index]); // build new permuted block into empty block
+        }
+
+    }
+
+    // putting it all together 
+    for (int i = 0; i < blocks.size(); i++) {
+        plain_text.append(blocks[i]); 
+    }
+
+    return plain_text;
 
 }
 
@@ -128,6 +194,12 @@ int main(){
     } 
 
     std::string encrypt = permutationCipher(plain_text, m, permutation); 
-    std::cout << "encrypted message: " << encrypt << std::endl; 
+    std::cout << "encrypted message: " << encrypt << std::endl;
+    
+    std::cout << "--------" << std::endl; 
+
+    // decryption 
+    std::string decrypt = permutationCipherDecryption(encrypt, m, permutation); 
+    std::cout << "decrypted message: " << decrypt << std::endl; 
 
 }
